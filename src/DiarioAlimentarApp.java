f568e0f701678f01deb15b17e14097bf11e95140
pedro <pmesquitabr@gmail.com>
2021-06-23 19:02:56 -0300

    criação inicial das telas

---
+++ src/DiarioAlimentarApp.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DiarioAlimentarApp extends Application {
+
+	
+	
+	
+	@Override 
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(ScreenManager.getInstance().getMainScene());
+        primaryStage.setTitle("Diário Alimentar");
+        
+        primaryStage.setWidth(850);
+        primaryStage.setHeight(680);
+        primaryStage.setResizable(false);
+        
+        ScreenManager.getInstance().setPrimaryStage(primaryStage);
+        
+        primaryStage.show();
+    }
+	
+	
+	
+	public static void main(String[] args) {
+		DiarioAlimentarApp.launch(args);
+	}
+
+}
---
+++ src/gui/ScreenManager.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javafx.scene.Scene;
+
+public class ScreenManager {
+	private static ScreenManager instance;
+    private Scene DiarioAlimentar;
+    private DiarioAlimentarController diarioAlimentarController;
+    
+    private Scene PlanoAlimentar;
+    private PlanoAlimentarController planoAlimentarController;
+    
+}
